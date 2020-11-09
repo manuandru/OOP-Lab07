12589f8394c68553c77d3c96c8f31f0ba1e7f02e
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2020-11-09 21:00:09 +0100

    Modified Nested2 using anonymous class

--- src/it/unibo/oop/lab/nesting2/OneListAcceptable.java
+++ src/it/unibo/oop/lab/nesting2/OneListAcceptable.java
@@ -1,18 +1,42 @@
 package it.unibo.oop.lab.nesting2;
 
-import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 
 public class OneListAcceptable<T> implements Acceptable<T> {
 
-	private List<T> list;
+	private List<T> acceptedList;
 	
 	public Acceptor<T> acceptor() {
-		return new AcceptorImpl<T>(new HashSet<>(list));
+		return new Acceptor<T>() {
+			
+			Iterator<T> actualAcceptedIterator = acceptedList.iterator();
+
+			public void accept(T newElement) throws ElementNotAcceptedException {
+				if (!actualAcceptedIterator.hasNext() 
+						|| !actualAcceptedIterator.next().equals(newElement)) {
+					throw new ElementNotAcceptedException(newElement);
+				}
+			}
+
+			public void end() throws EndNotAcceptedException {
+				if (actualAcceptedIterator.hasNext()) {
+					throw new EndNotAcceptedException();
+				}
+			}
+			
+		};
 	}
 
-	public OneListAcceptable(List<T> list) {
-		this.list = list;
+	public OneListAcceptable(List<T> acceptedList) {
+		this.acceptedList = acceptedList;
 	}
+
+
+	public String toString() {
+		return "OneListAcceptable [acceptedList=" + acceptedList + "]";
+	}
+	
+	
 	
 }
